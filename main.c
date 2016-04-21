42d53d8fb5de42b93dc3527318f81278b9ad276d
Nina <nina@nina.limelighter.org>
2016-04-21 14:09:08 -0700

    some main stuff and makefile

--- main.c
+++ main.c
@@ -2,43 +2,56 @@
 //#include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main()
 {
-  char fullCommand[80];
-  char command[80];
-  int option=0;
-  pid_t pid;
-  do {
-      printf("prompt> ");
-    fgets(fullCommand, sizeof fullCommand, stdin);
-    sscanf(fullCommand, "%s", command);  //get first command
+	char fullCommand[80];
+	char command[80];
+	char* arg = malloc(sizeof(char)*250);
+	int option=0;
+	int child_status;
+	pid_t pid;
+	do {
+		printf("prompt> ");
+		fgets(command, sizeof command, stdin);
+		strcpy(arg, command);
+		
 
-    if (strcmp(command, "quit") == 0){
-      option = 1;
-    }
-    else{
-      option = 2;
-    }
+		if (strcmp(command, "quit") == 0){
+			option = 1;
+		}
+		else{
+			option = 2;
+		}
 
-    switch(option){
-    case 1:
-      break;
-    case 2:
-      pid = fork();
-      if (pid == 0){
-          printf("child\n");
-      }
-      else if(pid == -1) {
-          printf("failed\n");
-      }
-      else{
-          printf("parent\n");
-      }
-      break;
-    }
-  }
-  while(option == 3 && option == 1);
+		switch(option){
+			case 1:
+				break;
+			case 2:
+				pid = fork();
+				if (pid == 0){
+					printf("child\n");
+					if(execvp(command, args) < 0) {
+						printf("failed\n");
+					}
+					//exit(0);
+				}
+				else if(pid == -1) {
+					printf("failed\n");
+				}
+				else{
+					waitpid(0, &child_status, 0);
+					if(WIFEXITED(child_status)) {
+						printf("Child terminated\n");
+					}
+					printf("parent\n");
+				}
+				break;
+			
+		}
+	}
+	while(option == 3 && option == 1);
 
-  return 0;
+	return 0;
 }
